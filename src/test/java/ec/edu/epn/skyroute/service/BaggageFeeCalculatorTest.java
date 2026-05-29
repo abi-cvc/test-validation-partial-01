89ebb6c66a4fcd2ee9dd92ed232a4b48491abd0d
abi-cvc <carol.velasquez@epn.edu.ec>
2026-05-29 17:51:53 -0500

    Validación de pasajero estandar añadiendo un beforeEach local usando Nested

--- src/test/java/ec/edu/epn/skyroute/service/BaggageFeeCalculatorTest.java
+++ src/test/java/ec/edu/epn/skyroute/service/BaggageFeeCalculatorTest.java
@@ -1,5 +1,7 @@
 package ec.edu.epn.skyroute.service;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -9,6 +11,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 public class BaggageFeeCalculatorTest {
@@ -42,5 +46,20 @@ public class BaggageFeeCalculatorTest {
     void shouldThrow_whenBagCountIsLessThanOne(int invalidCount) {
         assertThatIllegalArgumentException()
             .isThrownBy(() -> calculator.calculateFee(20.0, invalidCount, 1L));
+    }
+
+    @Nested
+    class StandarPassenger {
+        
+        @BeforeEach
+        void setup() {
+            when(passengerService.isVip(1L)).thenReturn(false);
+        }
+
+        @Test
+        void oneBag_normalWeight_shouldReturn30(){
+            double fee = calculator.calculateFee(20.0, 1, 1L);
+            assertThat(fee).isEqualTo(30.0);
+        }
     }
 }
